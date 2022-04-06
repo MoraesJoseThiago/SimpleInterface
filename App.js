0496e3b6a414a4fdc578888d94007f6e7bcaedaa
MoraesJoseThiago <moraesjosethiago@gmail.com>
2022-04-06 19:31:51 -0300

    FlexDirection CSS

--- App.js
+++ App.js
@@ -6,34 +6,53 @@ import {
   Button,
 } from 'react-native';
 
-const App = () => {
-  const [fofoca, setFofoca] = useState('Me atualize')
-
-  const onClickHandler = () => {
-    setFofoca('O Julio e chato pra crl!')
-  }
-
+const App =() =>{
   return (
     <View style={styles.body}>
-      <Text style={styles.text}>Já esta sabendo da fofoca?</Text>
-      <Text style={styles.text}>{fofoca}</Text>
-      <Button title='Me conta a fofoca' onPress={onClickHandler}></Button>
+      <View style={styles.view1}>
+      <Text style={styles.text}>1</Text>
+      </View>
+      <View style={styles.view2}>
+      <Text style={styles.text}>2</Text>
+      </View> 
+      <View style={styles.view3}>
+      <Text style={styles.text}>3</Text>
+      </View>
     </View>
   );
-};
+}
 
+ 
 const styles = StyleSheet.create({
   body: {
+  flex: 1,
+  backgroundColor: '#0000ff',
+  alignItems: 'stretch',
+  justifyContent: 'center',
+  {/* flexDirection: 'column-reverse', Faz reverter os numeros =  3,2,1*/}
+  },
+  view1: {
     flex: 1,
-    backgroundColor: '#0000ff',
+    backgroundColor: '#00ffff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  view2: {
+    flex: 1,
+    backgroundColor: '#ff00ff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  view3: {
+    flex: 1,
+    backgroundColor: '#ffff00',
     alignItems: 'center',
     justifyContent: 'center',
   },
   text: {
-    color: '#ffffff',
-    fontSize: 20,
-    fontStyle: 'italic',
-    margin: 10,
+    color: '#000000',
+    fontSize: 50,
+    margin: 30,
   },
 });
 
