eaac7859873944b16c3b76b664b55da7c371cf5d
MoraesJoseThiago <moraesjosethiago@gmail.com>
2022-05-04 19:25:32 -0300

    ghibli PT1

--- App.js
+++ App.js
@@ -1,50 +1,32 @@
-import React, { useState, useEffect } from 'react';
-import {
-  StyleSheet,
-  View,
-  Text,
-  ScrollView,
-  RefreshControl,
-} from 'react-native';
-import { FlatList } from 'react-native';
-import { useEffect } from 'react/cjs/react.production.min';
-
-const App = () => {
-  const[pokemon, setPokemon] = useState([])
-    useEffect(() => {
-      fetch('https://pokeapi.co/api/v2/pokemon',{
-        method: 'GET',
-        headers:{
-          'Accept': 'application/json'
-        }
-      })
-        .then(response => response.json())
-        .then(data=>{
-          console.log(data)
-          setPokemon(data.results)
+import React, {useEffect, useState} from 'react'
+import {SafeAreaView, View,FlatList,Image,Text} from 'react-native'
+export default function App(){
+  const [allMovies,setAllMovie] = useState([])
+  useEffect(()=>{
+    fetch('https://ghibliapi.herokuapp.com/films')
+    .then(response=>response.json())
+    .then(data=>{
+        data.forEach(movie=>{
+            console.log(movie.title);
+            console.log(movie.deion)
+            console.log(movie.release_date)
+            setAllMovie(movie)
         })
-    },[])
-    return(
-      <View>
-        <FlatList
-          data={pokemons}
-          keyExtracton={(pokemon)=>pokemon.name}
-          contentContainerStylee={{flexGrow: 1}}
-          renderItem={PokemonShow}
-        />
-      </View>
-    )
-};
-
-function PokemonShow(item){
-  const{name,url} = item.item
-  const pokemonNumber = url.replace('https://pokeapi.co/api/v2/pokemon', '')
-
-  const ImageUrl = 'https://cdn.traction.one/pokedex/pokemon'+ pokemonNumber+'.png'
+    }).catch(err=>{
+        console.log(err)
+    })
+  },[])
   return(
-  <View style={{flexDirection: 'row'}}>
-    <Nome>{name}</Nome>
-    <Imagem source={{ url: ImageUrl.replace('/.png','.png')}}/>
+    <View>
+      <FlatList
+        data={allMovies}
+        keyExtractor={(movie) => movie.title}
+        renderItem={MovieShow}
+      />
     </View>
   )
+}
+
+function MovieShow(item){
+  const{title,description,image} = item.item
 }
