2039ed7e381e6a329d7326cae02a296a51b10063
MoraesJoseThiago <moraesjosethiago@gmail.com>
2022-05-11 19:22:02 -0300

    Aula 11/05

--- App.js
+++ App.js
@@ -1,8 +1,11 @@
 import React, {useEffect, useState} from 'react'
-import {SafeAreaView, View,FlatList,Image,Text} from 'react-native'
+import {SafeAreaView, View,FlatList,Image,Text, StyleSheet} from 'react-native'
 export default function App(){
   const [allMovies,setAllMovie] = useState([])
   useEffect(()=>{
+    // fetch busca os dados do site
+    // then response vai transformar as respostas em um json
+    // then data vai formatar os dados mapeando o array
     fetch('https://ghibliapi.herokuapp.com/films')
     .then(response=>response.json())
     .then(data=>{
@@ -12,21 +15,39 @@ export default function App(){
             console.log(movie.release_date)
             setAllMovie(movie)
         })
+        setAllMovie(data); //data em vez de movie por causa que o arquivo do .then é uma variavel chamada data
     }).catch(err=>{
         console.log(err)
     })
   },[])
   return(
-    <View>
+    <View style={styles.body}>
+      <Text> Ghibli Movies </Text>
       <FlatList
         data={allMovies}
-        keyExtractor={(movie) => movie.title}
-        renderItem={MovieShow}
+        renderItem={({item})=>
+        <View>
+          <Text style={styles.Text}>{item.title}</Text>
+          <Image
+          style={{width: 50, height: 50}}
+            source={{uri: item.image}}
+          />
+        </View>
+        }
       />
     </View>
-  )
+  )   
 }
 
-function MovieShow(item){
-  const{title,description,image} = item.item
-}
+const styles = StyleSheet.create ({
+  body: {
+    backgroundColor: '#00ff',
+    alignItems: 'center'
+  },
+  Text:{
+    color: '#fff',
+    fontSize:20,
+    fontStyle: 'italic'
+  }
+ 
+});
